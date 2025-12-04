9040776fb9ff9c0f5f794063d2428928f7dcb1e2
FALANSSA <153799248+FALANSSA@users.noreply.github.com>
2025-12-05 00:11:50 +0300

    Add files via upload

--- cnt-group/src/app/@theme/components/menu/services/menu.service.ts
+++ cnt-group/src/app/@theme/components/menu/services/menu.service.ts
@@ -1,83 +1,105 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { MenuItem } from '../../models/menu.model';
-import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
-menuItems: MenuItem[] = [
-          {
-            idItem: 1,
-            itemName: 'Главная',
-            itemLink: 'mainPage',
-            iconTypeId: 1,
-            icon: null,
-            itemOrder: 1,
-            parentItem: null,
-            showSubMenu: false,
-            subMenuItems: []
-          },
-          {
-            idItem: 3,
-            itemName: 'Мониторинг',
-            itemLink: 'NULL',
-            iconTypeId: 1,
-            icon: null,
-            itemOrder: null,
-            parentItem: 5,
-            showSubMenu: false,
-            subMenuItems: []
-          },
-          {
-            idItem: 5,
-            itemName: 'Dashboard',
-            itemLink: 'dashboard',
-            iconTypeId: 1,
-            icon: '',
-            itemOrder: 3,
-            parentItem: 1,
-            showSubMenu: false,
-            subMenuItems: []
-          },
-          {
-            idItem: 7,
-            itemName: 'Админ Панель',
-            itemLink: 'administration',
-            iconTypeId: 2,
-            icon: null,
-            itemOrder: 4,
-            parentItem: null,
-            showSubMenu: false,
-            subMenuItems: []
-          },
-          {
-            idItem: 8,
-            itemName: 'Пользователи',
-            itemLink: 'administration/users',
-            iconTypeId: 1,
-            icon: '',
-            itemOrder: 1,
-            parentItem: 7,
-            showSubMenu: false,
-            subMenuItems: []
-          }
-        ];
+  menuItems: MenuItem[] = [
+    {
+      idItem: 1,
+      itemName: 'Главная',
+      itemLink: 'mainPage',
+      iconTypeId: 1,
+      icon: null,
+      itemOrder: 1,
+      parentItem: null,
+      showSubMenu: false,
+      subMenuItems: []
+    },
+    {
+      idItem: 2,
+      itemName: 'Статистика',
+      itemLink: null,
+      iconTypeId: 1,
+      icon: null,
+      itemOrder: 2,
+      parentItem: null,
+      showSubMenu: false,
+      subMenuItems: []
+    },
+    {
+      idItem: 3,
+      itemName: 'Мониторинг',
+      itemLink: 'NULL',
+      iconTypeId: 1,
+      icon: null,
+      itemOrder: null,
+      parentItem: 5,
+      showSubMenu: false,
+      subMenuItems: []
+    },
+    {
+      idItem: 5,
+      itemName: 'Dashboard',
+      itemLink: 'dashboard',
+      iconTypeId: 1,
+      icon: '',
+      itemOrder: 3,
+      parentItem: 1,
+      showSubMenu: false,
+      subMenuItems: []
+    },
+    {
+      idItem: 7,
+      itemName: 'Админ Панель',
+      itemLink: 'administration',
+      iconTypeId: 2,
+      icon: null,
+      itemOrder: 4,
+      parentItem: null,
+      showSubMenu: false,
+      subMenuItems: []
+    },
+    {
+      idItem: 8,
+      itemName: 'Пользователи',
+      itemLink: 'administration/users',
+      iconTypeId: 1,
+      icon: '',
+      itemOrder: 1,
+      parentItem: 7,
+      showSubMenu: false,
+      subMenuItems: []
+    },
+    {
+      idItem: 9,
+      itemName: 'Общая',
+      itemLink: 'statistics/general',
+      iconTypeId: 1,
+      icon: '',
+      itemOrder: 1,
+      parentItem: 2,
+      showSubMenu: false,
+      subMenuItems: []
+    }
+  ];
 
-private currentItemSubject = new BehaviorSubject<MenuItem>(this.menuItems[0]);
-currentItem$ = this.currentItemSubject.asObservable();
+  private currentItemSubject = new BehaviorSubject<MenuItem>(this.menuItems[0]);
+  currentItem$ = this.currentItemSubject.asObservable();
 
-getParentItems(): MenuItem[] {
-  return this.menuItems.filter(item => item.parentItem === null);
+  constructor() { }
+
+  getParentItems(): MenuItem[] {
+    return this.menuItems.filter(item => item.parentItem === null);
+  }
+
+  getChildrenItems(selectedItem: MenuItem): MenuItem[] {
+    return this.menuItems.filter(subItem => subItem.parentItem === selectedItem.idItem);
+  }
+
+  setCurrentItem(item: MenuItem): void {
+    this.currentItemSubject.next(item);
+  }
 }
-
-getChildrenItems(selectedItem: MenuItem): MenuItem[] {
-  return this.menuItems.filter(subItem => subItem.parentItem === selectedItem.idItem);
-}
-
-setCurrentItem(item: MenuItem): void {
-  this.currentItemSubject.next(item);
-
-}
-}
-
