cf6d4d01fe03da3661e16fb4f6e14324ae200849
JustcrazyLeo <153904086+JustcrazyLeo@users.noreply.github.com>
2025-12-05 23:09:47 +0300

    Add files via upload

--- cnt-group/src/app/app.module.ts
+++ cnt-group/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RouterModule } from '@angular/router';
 import { MenuParentsComponent } from './@theme/components/menu/menu-parents/menu-parents/menu-parents.component';
 import { CommonModule } from '@angular/common';
 import { MenuChildrensComponent } from './@theme/components/menu/menu-childrens/menu-childrens/menu-childrens.component';
+import { ThemeSwitcherComponent } from './@theme/components/header/theme-switcher/theme-switcher.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { MenuChildrensComponent } from './@theme/components/menu/menu-childrens/
     FooterComponent,
     SidebarComponent,
     MenuParentsComponent,
-    MenuChildrensComponent
+    MenuChildrensComponent,
+    ThemeSwitcherComponent
   ],
   imports: [
     BrowserModule,
