36d75694d4c4346dba86b7f5e5febee0ea7cf1b5
JustcrazyLeo <153904086+JustcrazyLeo@users.noreply.github.com>
2025-12-08 16:25:24 +0300

    Add files via upload

--- cnt-group/src/app/@theme/components/header/theme-switcher/theme-switcher.component.ts
+++ cnt-group/src/app/@theme/components/header/theme-switcher/theme-switcher.component.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { ThemeSwitcherService } from './services/theme-switcher.service';
+import { Component } from '@angular/core';
+import { ThemeSwitcherService, ThemeMode } from './services/theme-switcher.service';
 
 @Component({
   selector: 'app-theme-switcher',
   templateUrl: './theme-switcher.component.html',
   styleUrls: ['./theme-switcher.component.css']
 })
-export class ThemeSwitcherComponent implements OnInit {
-  currentTheme: string = this.themeService.currentTheme || 'light-theme';
+export class ThemeSwitcherComponent {
+  currentTheme: ThemeMode;
 
-  constructor(private themeService: ThemeSwitcherService) {}
-
-  ngOnInit() {
-    this.themeService.setTheme(this.currentTheme);
+  constructor(private themeService: ThemeSwitcherService) {
+    this.currentTheme = this.themeService.currentTheme;
   }
 
-  toggleTheme() {
-    this.currentTheme = this.currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
-    this.themeService.setTheme(this.currentTheme);
+  toggleTheme(): void {
+    const button = document.querySelector('.theme-switcher__button');
+    if (button) {
+      button.classList.add('theme-switch-animate');
+
+      const newTheme = this.themeService.toggleTheme();
+      this.currentTheme = newTheme;
+
+      setTimeout(() => {
+        button.classList.remove('theme-switch-animate');
+      }, 600);
+    } else {
+      const newTheme = this.themeService.toggleTheme();
+      this.currentTheme = newTheme;
+    }
+  }
+
+  getThemeTitle(): string {
+    return this.themeService.getThemeLabel();
   }
 }
